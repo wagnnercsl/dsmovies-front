9ec073e69a2b208840c5fffdc966ecfd545b5c21
Wagnner Leivas <wagnner.leivas@gmail.com>
2022-01-15 20:10:05 -0300

    Add Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ReactComponent as GithubIcon } from '../../assets/github-icon.svg';
+import './styles.css';
+
+const Navbar = () => {
+    return(
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovies</h1>
+                <a href="https://github.com/wagnnercsl" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon fontSize={50} />
+                        <p className="dsmovie-contact-link">/wagnnercsl</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
